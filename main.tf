38a76c89eef9f865fa933c9692a3d93080c008f1
akamsoft <alexkam75@yandex.ru>
2026-02-16 00:01:29 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+}
+
+provider "docker" {
+  host = "ssh://vm-yandex"
+}
+
+resource "random_password" "mysql_root" {
+  length  = 16
+  special = true
+}
+
+resource "random_password" "mysql_user" {
+  length  = 16
+  special = true
+}
+
+resource "docker_image" "mysql" {
+  name = "mysql:8"
+}
+
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name  = "mysql-wordpress"
+
+  ports {
+    internal = 3306
+    external = 3306
+    ip     = "127.0.0.1"
+  }
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.mysql_root.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress", 
+    "MYSQL_PASSWORD=${random_password.mysql_user.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+  restart = "always"
+}
